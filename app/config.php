a55c054db6790c7206259963d4dae8bd1fa2d79b
jegoncu <jegoncu@gmail.com>
2024-11-26 10:56:42 +0100

    modificado el config.php e historico.txt

--- app/config.php
+++ app/config.php
@@ -2,10 +2,10 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentos2020";
-     static public $mvc_bd_usuario  = "myjesus95";
-     static public $mvc_bd_clave    = "Q1GuQDIX";
+     static public $mvc_bd_hostname = "185.42.105.24";
+     static public $mvc_bd_nombre   = "alimentos32024";
+     static public $mvc_bd_usuario  = "myalimento23";
+     static public $mvc_bd_clave    = "icu98Nfb";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
